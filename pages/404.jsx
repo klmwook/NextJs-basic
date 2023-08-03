5781aab2bfc4e072d07b34fa875f982b9acb68ea
klmwook <klmwook72@gmail.com>
2023-08-03 11:14:41 +0900

    404 페이지 커스텀마이징

---
+++ pages/404.jsx
@@ -0,0 +1,11 @@
+import SubLayout from '@/components/SubLayout';
+
+function NotFound() {
+	return (
+		<SubLayout name={'404'}>
+			<h1>404페이지 입니다.</h1>
+		</SubLayout>
+	);
+}
+
+export default NotFound;
