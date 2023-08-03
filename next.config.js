d540b211a45a79514eed8269a5d2aaa9ae942232
klmwook <klmwook72@gmail.com>
2023-08-03 10:06:21 +0900

    Nextjs 기초 세팅

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+//전역으로 쓰고싶은 sass를 위해서 sassOptions을 추가 하였음
+const path = require('path');
+const nextConfig = {
+	reactStrictMode: false,
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+		prependData: `@import "styles/_variables.scss";`,
+	},
+};
+
+module.exports = nextConfig;
