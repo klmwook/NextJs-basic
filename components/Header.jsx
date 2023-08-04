8b95e3d36bd27a222fd6639ce6059d780f379e05
klmwook <klmwook72@gmail.com>
2023-08-04 09:52:10 +0900

    nextjs에서 font 적용

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import styles from '@/styles/Header.module.scss';
+import styles from './Header.module.scss';
 console.log(styles);
 
 function Header() {
