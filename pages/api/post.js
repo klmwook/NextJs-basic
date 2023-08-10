3567bee7be1cd869d2ddc2c47beff813521955e2
klmwook <klmwook72@gmail.com>
2023-08-10 11:09:26 +0900

    주석 정리 및 데이터 저장 작업 순서 정리

--- pages/api/post.js
+++ pages/api/post.js
@@ -1,16 +1,20 @@
 import { connectMongoDB } from '@/libs/MongoConnect';
 
-//안쪽에서 await로 동기화 처리할 예정이므로 wrapping함수에 async지정
 export default async function handler(req, res) {
-	//await로 동기화하므로 then,catch문을 쓸수 없으니 try, catch문으로 예외처리
 	try {
-		//요청 성공시 실행될 구문
 		const DB = await connectMongoDB();
-		console.log(DB);
 		res.status(200).send('success');
 	} catch (err) {
-		//요청 실패시 실행될 구문
-		console.log(err);
 		res.status(400).send({ err });
 	}
 }
+
+/*
+  데이터 저장 작업 순서
+  1. 저장할 데이터의 구조에 맞게 스키마 생성
+  2. api 라우터에서 스키마 적용된 모델 객체에 Client로부터 전달 받은 데이터를 바인딩 후 DB에 저장한 뒤, 응답성공 Client로 전송
+    1) Client에서 Post 요청 받음
+    2) 카운터 모델에서 communityNum 가져온 뒤 Client로부터 전달받은 객체와 결합
+    3) 결합된 객체를 Post 모델로 저장
+    4) 저장이 완료되면 다시 Counter 값을 1 증가
+*/
